4d1862f8bca9445adf6555e13f694f4e783bd219
dhavalprajapati1503 <dp353135@gmail.com>
2025-01-18 12:04:36 +0530

    Add files via upload

---
+++ RealHome_Main/js/script.js
@@ -0,0 +1,16 @@
+//Back to Top Button start
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+//Back to Top Button End
